63208d399766f4d1e05f3ce2cc406f7a3f7d10a7
Alex Knoll <alexandergknoll@gmail.com>
2015-10-15 09:16:09 -0400

    add Figaro gem for local keystore to manage Spotify secrets

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # App-specific stuff
 gem 'rspotify'
+gem "figaro"
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+RSpotify::authenticate(ENV["spotify_client_id"], ENV["spotify_secret"])
+
 module JukePro
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
