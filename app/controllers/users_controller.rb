64b4b2ee78325aab9fd97991d330c73e48bb133f
Alex Knoll <alexandergknoll@gmail.com>
2015-10-16 12:08:57 -0400

    add landing page for authenticated users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-    def spotify
-    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+  def spotify
+    @user = RSpotify::User.new(request.env['omniauth.auth'])
     # Now you can access user's private data, create playlists and much more
   end
 end
