3ef49ce8666bf3ea4b29d1749bba5aee73bec10c
Erik Stockmeier <erik.d.stock@gmail.com>
2015-10-18 23:10:21 -0500

    make a nice loop so authentication is impossible

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,27 @@
 class UsersController < ApplicationController
+  
   def spotify
-    @user = RSpotify::User.new(request.env['omniauth.auth'])
+    this_user = RSpotify::User.new(request.env['omniauth.auth'])
+
+    @user = User.find_or_create_by(spotify_id: this_user.id)
+    # byebug
+
+    # set user properties - RSpotify::Users can't save... :/
+
+    begin
+    	@user.image = this_user.images[0].url
+    	@user.email = this_user.email
+    	@user.profile_url = this_user.external_urls.spotify
+    	@user.display_name = this_user.display_name 
+
+    rescue => e
+    	warn "Error setting user data: #{e}"
+    ensure
+    	@user.save
+    end
     # Now you can access user's private data, create playlists and much more
+    # woot
+    redirect_to :root
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'application#index'
   get '/auth/spotify/callback', to: 'users#spotify'
 
+  
+
 end
