f08bce7fed2df1a05bb19da700a1355b7011ecf5
Alex Knoll <alexandergknoll@gmail.com>
2015-10-19 11:32:32 -0400

    refactor spotify callback and add class method to handle setting omniauth parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,11 @@
 class UsersController < ApplicationController
 	skip_before_action :authenticate_user!, only: [:spotify]
-
   
   def spotify
-  	puts "in the callback..."
-    this_user = RSpotify::User.new(request.env['omniauth.auth'])
-
-    @user = User.find_or_create_by(spotify_id: this_user.id)
-
-    # Better way to refactor this like with a special spotify_params method perhaps? This error rescue might not be necessary.
-    begin
-    	@user.image = this_user.images[0].url
-    	@user.email = this_user.email
-    	@user.profile_url = this_user.external_urls.spotify
-    	@user.display_name = this_user.display_name 
-    rescue => e
-    	warn "Error setting user data: #{e}"
-    ensure
-    	if @user.save
-    		# session[:user_id] = @user.id
-    	else
-    		flash[:alert] = "You didn't sign in right :/"
-    	end
-    end
-   	session[:user_id] = @user.id
-   	p session
+    spotify_params = request.env['omniauth.auth']
+    user = User.from_omniauth(spotify_params)
+   	session[:user_id] = user.id if user
     redirect_to :root
   end
-
-
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  def self.from_omniauth(spotify_params)
+    spotify_user = RSpotify::User.new(spotify_params)
+    where(spotify_id: spotify_user.id).first_or_create do |user|
+      user.image = spotify_user.images[0].url
+      user.email = spotify_user.email
+      user.profile_url = spotify_user.external_urls.spotify
+      user.display_name = spotify_user.display_name 
+    end
+  end
 end
