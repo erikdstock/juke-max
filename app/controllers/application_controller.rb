74eda6d3841a16e6bb16e84bc98136bcd1a6dd3a
Erik Stockmeier <erik.d.stock@gmail.com>
2015-10-19 09:48:27 -0500

    authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!, except: [:index]
+  skip_before_action :authenticate_user, only: [:index]
+  
+  helper_method :current_user
+
+  def index
+  	current_user
+  
+  end
+
+  private
+
+  def current_user
+  	@_current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def authenticate_user!
+  	puts "Authenticating..."
+  	redirect_to :root unless current_user
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,33 @@
 class UsersController < ApplicationController
+	skip_before_action :authenticate_user!, only: [:spotify]
+
   
   def spotify
+  	puts "in the callback..."
     this_user = RSpotify::User.new(request.env['omniauth.auth'])
 
     @user = User.find_or_create_by(spotify_id: this_user.id)
-    # byebug
 
-    # set user properties - RSpotify::Users can't save... :/
-
+    # Better way to refactor this like with a special spotify_params method perhaps? This error rescue might not be necessary.
     begin
     	@user.image = this_user.images[0].url
     	@user.email = this_user.email
     	@user.profile_url = this_user.external_urls.spotify
     	@user.display_name = this_user.display_name 
-
     rescue => e
     	warn "Error setting user data: #{e}"
     ensure
-    	@user.save
+    	if @user.save
+    		# session[:user_id] = @user.id
+    	else
+    		flash[:alert] = "You didn't sign in right :/"
+    	end
     end
-    # Now you can access user's private data, create playlists and much more
-    # woot
+   	session[:user_id] = @user.id
+   	p session
     redirect_to :root
   end
+
+
 
 end
