36be9080e69add2e5b31b96968035495ab65a18d
Erik Stockmeier <erik.d.stock@gmail.com>
2015-10-18 22:03:06 -0500

    regen rails bin (prob did nothing) and move secrets.yml to application for figaro management

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module JukePro
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.secret_key_base = ENV['SECRET_KEY_BASE']
+
   end
 end
 
