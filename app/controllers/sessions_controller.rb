8ab367ac71f7dbd1aedcd80cc50a738cc7cb2c1d
Alex Knoll <alexandergknoll@gmail.com>
2015-10-20 15:58:13 -0400

    create sessions controller and move create method/route to sessions rather than users controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:create]
+  
+  def create
+    spotify_params = request.env['omniauth.auth']
+    user = User.from_omniauth(spotify_params)
+    session[:user_id] = user.id if user
+    redirect_to :root
+  end
+
+  def destroy
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
   root to: 'application#index'
-  get '/auth/spotify/callback', to: 'users#spotify'
-
-  
+  get '/auth/spotify/callback', to: 'sessions#create'
 
 end
