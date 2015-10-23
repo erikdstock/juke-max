db8547ec522732ead55992e5805d092d5875680b
Alex Knoll <alexandergknoll@gmail.com>
2015-10-22 22:11:46 -0400

    validate presence of name for playlist and create user playlist before creating playlist

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,9 @@
 class Playlist < ActiveRecord::Base
   belongs_to :user
+  validates :name, presence: true
+  before_create :spotify_create
+
+  def spotify_create
+    self.user.spotify.create_playlist!(name)
+  end
 end
